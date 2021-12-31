f2a41d8d72d829d8ed82d8c173d60d4ced907293
simonecolonna <s.colonna92@gmail.com>
2021-12-31 15:13:55 +0100

    fix

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup, find_packages
 setup(name='DiggerPy',
       version='0.1',
       description='to fill...',
-    #   url='http://github.com/...',
+      # url='http://github.com/...',
       author='SimCo',
       author_email='si.colonna92@gmail.com',
       # package_dir={"": "diggerpy"},
       packages=find_packages(),
       install_requires=[
           # 'pandas',
-          'json'
           'oauth2',
           'requests',
       ],
